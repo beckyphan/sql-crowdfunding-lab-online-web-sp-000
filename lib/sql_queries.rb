14beb417dd11b100864ab1587fe1c66cfcb47571
Becky Phan <beckyphan@live.com>
2019-07-22 18:36:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.name, pledges.amount 
-FROM projects, pledges
+FROM projects
 LEFT JOIN projects
 ON Project.id = Pledge.project_id
 ORDER BY Projects.name ASC;"
