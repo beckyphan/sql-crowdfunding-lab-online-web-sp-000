b51933f9c04fbddfc34e55dc4f73f91c5426f46e
Becky Phan <beckyphan@live.com>
2019-07-22 18:25:03 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,4 +6,9 @@ CREATE TABLE users (
   age INTEGER);
   
 CREATE TABLE projects (
-  )
+  id INTEGER PRIMARY KEY, 
+  title TEXT, 
+  category TEXT, 
+  funding_goal INTEGER, 
+  start_date TEXT, 
+  end_date TEXT);
