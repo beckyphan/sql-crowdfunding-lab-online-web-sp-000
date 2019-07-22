60d1974ed77103e736838e259206fd1be5e25c96
Becky Phan <beckyphan@live.com>
2019-07-22 18:36:21 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT Projects.name, Pledges.amount 
+"SELECT projects.name, pledges.amount 
 FROM projects, pledges
 LEFT JOIN projects
 ON Project.id = Pledge.project_id
