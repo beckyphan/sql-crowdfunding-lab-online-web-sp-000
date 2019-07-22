abbb846ff07f5d31fddb5773e330aee86d75f034
Becky Phan <beckyphan@live.com>
2019-07-22 19:29:07 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ FROM Users
 INNER JOIN Pledges
 ON User.id = Pledges.user_id
 GROUP BY User.id
-ORDER BY Projects.title ASC;"
+ORDER BY user.name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
