5040c7bb180eef29061b2df6fd70a5b60cc00d5b
Becky Phan <beckyphan@live.com>
2019-07-22 19:30:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,12 @@ ORDER BY Users.name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT Users.name, Users.age, SUM(Pledges.amount)
+FROM Users
+INNER JOIN Pledges
+ON Users.id = Pledges.user_id
+GROUP BY Users.id
+ORDER BY Users.name ASC;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
