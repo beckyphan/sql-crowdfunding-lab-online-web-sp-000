6d5bfbbcd23fc3cf568971dd585409c674f35b0f
Becky Phan <beckyphan@live.com>
2019-07-22 20:13:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,11 @@ ORDER BY Users.name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-""
+"SELECT Projects.title, DIFFERENCE(Project.funding_goal, SUM(Pledges.amount))
+FROM Projects
+INNER JOIN Pledges
+ON Projects.id = Pledges.project_id
+GROUP BY Projects.id;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
